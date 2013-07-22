df92c9af5195bbf1286d42d159e0bb055b763936
Simon Menke <simon.menke@gmail.com>
2013-07-22 14:06:49 +0200

    First commit

---
+++ run.sh
@@ -0,0 +1,19 @@
+
+if [ ! -n "$WERCKER_GIT_MERGE_SOURCE" ]; then
+  fail 'Please specify source property'
+fi
+
+if [ ! -n "$WERCKER_GIT_MERGE_TARGET" ]; then
+  fail 'Please specify target property'
+fi
+
+cd "$WERCKER_ROOT" || fail "could not change directory to source_dir \"$WERCKER_ROOT\""
+
+git remote update origin || fail "could not fetch branches from the remote"
+
+git checkout -b "$WERCKER_GIT_MERGE_TARGET" "origin/$WERCKER_GIT_MERGE_TARGET" || fail "could not checkout $WERCKER_GIT_MERGE_TARGET"
+
+git merge --no-ff "$WERCKER_GIT_MERGE_SOURCE" || fail "could not merge $WERCKER_GIT_MERGE_SOURCE into $WERCKER_GIT_MERGE_TARGET"
+
+gir push origin "$WERCKER_GIT_MERGE_TARGET" || fail "could not push $WERCKER_GIT_MERGE_TARGET"
+
