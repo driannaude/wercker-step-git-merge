dd59c489fb5cf87d45018f42c8e742964c128102
Drian Naude <drian.naude@truescape.com>
2016-06-01 15:20:23 +1200

    Set entry point on checkout

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ cd "$WERCKER_ROOT" || fail "could not change directory to source_dir \"$WERCKER_
 
 git remote update origin || fail "could not fetch branches from the remote"
 
-git checkout -B "$WERCKER_GIT_MERGE_TARGET" || fail "could not checkout $WERCKER_GIT_MERGE_TARGET"
+git checkout -B "$WERCKER_GIT_MERGE_TARGET" "origin/$WERCKER_GIT_MERGE_TARGET" || fail "could not checkout $WERCKER_GIT_MERGE_TARGET"
 
-git merge --no-ff "$WERCKER_GIT_MERGE_SOURCE" || fail "could not merge $WERCKER_GIT_MERGE_SOURCE into $WERCKER_GIT_MERGE_TARGET"
+git merge --no-ff "origin/$WERCKER_GIT_MERGE_SOURCE" || fail "could not merge $WERCKER_GIT_MERGE_SOURCE into $WERCKER_GIT_MERGE_TARGET"
 
 git push origin "$WERCKER_GIT_MERGE_TARGET" || fail "could not push $WERCKER_GIT_MERGE_TARGET"
